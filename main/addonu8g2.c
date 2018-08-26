f387d6a3172800350fc4efbeb55cf5383585f4b3
emax73 <emax@cosmostat.com>
2018-08-26 11:03:43 +0300

    Erase &amp; string at display

--- main/addonu8g2.c
+++ main/addonu8g2.c
@@ -235,7 +235,7 @@ u8g2_SendBuffer(&u8g2);
 
 //Max
 void eraseSlashes(char * str) {
-	//Symbols: \" \' \\ \? \/
+	//Symbols: \" \' \\ \? \/ &amp;
 	char * sym = str, * sym1;
 	if (str != NULL) {
 		while (*sym != 0) {
@@ -245,7 +245,18 @@ void eraseSlashes(char * str) {
 					*sym = 0x1f; //Erase \ to non-printable symbol
 					sym++;
 				}	
-			} 
+			} else if (*sym == '&') {
+				if (sym[1] == 'a' && sym[2] == 'm' && sym[3] == 'p' && sym[4] == ';') {
+					sym++;
+					*sym = 0x1f;
+					sym++;
+					*sym = 0x1f;
+					sym++;
+					*sym = 0x1f;
+					sym++;
+					*sym = 0x1f;
+				}
+			}  
 			sym++;
 		}
 	} 	
